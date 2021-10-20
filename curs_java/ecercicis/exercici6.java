fbb38cc4135a66b54dc7c319401b08a45b4bb43d
portlyncon <59870519+portlyncon@users.noreply.github.com>
2021-10-20 20:32:03 +0200

    fin

---
+++ curs_java/ecercicis/exercici6.java
@@ -0,0 +1,51 @@
+
+package curs_java.ecercicis;
+
+import java.util.Scanner;
+
+public class exercici6 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // creamos los arrays
+        String[] empleados = new String[10];
+        double[] sueldos = new double[10];
+
+        // variables donde guardar el nombre y sueldo del empleado que más gana
+        String nombreMayor;
+        double mayorSueldo;
+
+        int i = 0;
+
+        // se lee el primer empleado
+        System.out.println("Lectura de nombres y sueldos de empleados: ");
+        System.out.print("Empleado " + (i + 1) + ": ");
+        empleados[i] = sc.nextLine();
+        System.out.print("Sueldo: ");
+        sueldos[i] = sc.nextDouble();
+
+        // se toma el primero leído como mayor
+        mayorSueldo = sueldos[i];
+        nombreMayor = empleados[i];
+
+        // se leen el resto de empleados
+        for (i = 1; i < empleados.length; i++) {
+            sc.nextLine(); // limpiar el buffer
+            System.out.print("Empleado " + (i + 1) + ": ");
+            empleados[i] = sc.nextLine();
+            System.out.print("Sueldo: ");
+            sueldos[i] = sc.nextDouble();
+            // se compara el sueldo leído con el mayor actual
+            if (sueldos[i] > mayorSueldo) {
+                mayorSueldo = sueldos[i];
+                nombreMayor = empleados[i];
+            }
+        }
+
+        // mostrar resultados
+        System.out.println("Empleado con mayor sueldo: " + nombreMayor);
+        System.out.println("Sueldo: " + mayorSueldo);
+    }
+}
