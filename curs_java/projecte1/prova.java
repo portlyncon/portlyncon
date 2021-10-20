fb831fc42eb0cfce85e475bd52216fd080e48ef4
portlyncon <59870519+portlyncon@users.noreply.github.com>
2021-10-20 18:27:46 +0200

    prova

--- curs_java/projecte1/prova.java
+++ curs_java/projecte1/prova.java
@@ -1,11 +1,25 @@
 package curs_java.projecte1;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class prova {
 
     public static void main(String arg[]) {
 
         System.out.println("provaaaaaaaaaaaaaaaaa");
 
+        int[] numeros1 = { 3, 7, 8, 9 };
+        int[] numeros2 = numeros1;
+        // LA MISMA POSICION DE MEMORIA
+        numeros2[2] = 90;
+        System.out.println(numeros1);
+        System.out.println(numeros2);
+
+        Stream.of(numeros1).forEach((item) -> System.out.println(item));
+        // Listarelementos
+        Arrays.stream(numeros1).forEach((item) -> System.out.println(item));
     }
 
 }
