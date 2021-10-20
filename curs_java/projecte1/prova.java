59ca43a0c3092e0d491b4dfcd7749752371e8b33
portlyncon <59870519+portlyncon@users.noreply.github.com>
2021-10-20 18:06:25 +0200

    confirmar

--- curs_java/projecte1/prova.java
+++ curs_java/projecte1/prova.java
@@ -4,7 +4,7 @@ public class prova {
 
     public static void main(String arg[]) {
 
-        System.out.println("holaaaaaaaaaaaaaaaaaaa");
+        System.out.println("provaaaaaaaaaaaaaaaaa");
 
     }
 
