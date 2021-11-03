7ef7797a83cea6ad976defd668fb248743c309ea
portlyncon <59870519+portlyncon@users.noreply.github.com>
2021-11-03 19:22:15 +0100

    aa

--- curs_java/ecercicis/exercicis_String/exercici2.java
+++ curs_java/ecercicis/exercicis_String/exercici2.java
@@ -11,9 +11,11 @@ public class exercici2 {
         for (String password : passwords) {
             checkSecurity(password);
         }
-
+        sc.close();
     }
 
+    // metode per comprovar les caraceristiques
+    // es van sumant 1 per cada caracteristica de la passw que compleix
     private static void checkSecurity(String password) {
         int securityCounter = 0;
         if (password.length() >= 8) {
@@ -31,7 +33,7 @@ public class exercici2 {
         if (checkNoNumberNoUpperNoLower(password)) {
             securityCounter++;
         }
-
+        // es comprova quantes caracteristiques es compleixen i es dona el resultat
         if (password.length() < 4 || securityCounter < 3) {
             System.out.println("\"" + password + "\" es una contraseña débil.");
         } else if (securityCounter == 3 || securityCounter == 4) {
@@ -43,10 +45,10 @@ public class exercici2 {
 
     private static boolean checkLower(String password) {
         boolean encontrada = false;
-        for (char c : password.toCharArray()) {
+        for (char c : password.toCharArray()) {// per passar el string a un array de chars .toCharArray()
             if (Character.isLowerCase(c)) {
                 encontrada = true;
-                break;
+                break; // per sortir de bucle for
             }
         }
         return encontrada;
@@ -54,10 +56,10 @@ public class exercici2 {
 
     private static boolean checkUpper(String password) {
         boolean encontrada = false;
-        for (char c : password.toCharArray()) {
+        for (char c : password.toCharArray()) { // per passar el string a un array de chars .toCharArray()
             if (Character.isUpperCase(c)) {
                 encontrada = true;
-                break;
+                break;// per sortir de bucle for
             }
         }
         return encontrada;
@@ -65,10 +67,10 @@ public class exercici2 {
 
     private static boolean checkNumber(String password) {
         boolean encontrada = false;
-        for (char c : password.toCharArray()) {
+        for (char c : password.toCharArray()) {// per passar el string a un array de chars .toCharArray()
             if (Character.isDigit(c)) {
                 encontrada = true;
-                break;
+                break;// per sortir de bucle for
             }
         }
         return encontrada;
@@ -79,7 +81,7 @@ public class exercici2 {
         for (char c : password.toCharArray()) {
             if (!Character.isDigit(c) && !Character.isLowerCase(c) && !Character.isUpperCase(c)) {
                 encontrada = true;
-                break;
+                break;// per sortir de bucle for
             }
         }
         return encontrada;
