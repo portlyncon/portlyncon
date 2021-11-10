6e31d286bd92fdc746c027787873ce9709af4008
portlyncon <59870519+portlyncon@users.noreply.github.com>
2021-11-10 21:59:04 +0100

    ffdfdf

--- curs_java/ecercicis/herencia/exercici1/cuadrat.java
+++ curs_java/ecercicis/herencia/exercici1/cuadrat.java
@@ -2,8 +2,19 @@ package curs_java.ecercicis.herencia.exercici1;
 
 public class cuadrat implements interface_poligon {
 
-    public cuadrat(int a, int b, int c, int d) {
+    int[] vertx_a = { 0, 0 };;
+    int[] vertx_b = { 0, 0 };;
+    int[] vertx_c = { 0, 0 };;
+    int[] vertx_d = { 0, 0 };;
+    int perimetre;
+    int area;
+    int distancia;
 
+    public cuadrat(int[] a, int[] b, int[] c, int[] d) {
+        vertx_a = a;
+        vertx_b = b;
+        vertx_c = c;
+        vertx_d = d;
     }
 
     @Override
@@ -17,4 +28,18 @@ public class cuadrat implements interface_poligon {
         // TODO Auto-generated method stub
 
     }
+
+    public int calcular_distancia(int p1, int p2) {
+        return distancia;
+
+    }
+
+    public int getPerimetre() {
+        return perimetre;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
 }
--- curs_java/ecercicis/herencia/exercici1/triangle.java
+++ curs_java/ecercicis/herencia/exercici1/triangle.java
@@ -2,8 +2,17 @@ package curs_java.ecercicis.herencia.exercici1;
 
 public class triangle implements interface_poligon {
 
-    public triangle(int a, int b, int c) {
+    int[] vertx_a = { 0, 0 };
+    int[] vertx_b = { 0, 0 };;
+    int[] vertx_c = { 0, 0 };;
+    int perimetre;
+    int area;
+    int distancia;
 
+    public triangle(int[] a, int[] b, int[] c) {
+        vertx_a = a;
+        vertx_b = b;
+        vertx_c = c;
     }
 
     @Override
@@ -16,6 +25,19 @@ public class triangle implements interface_poligon {
     public void met_Perimetre() {
         // TODO Auto-generated method stub
 
+    }
+
+    public int calcular_distancia(int p1, int p2) {
+        return distancia;
+
+    }
+
+    public int getPerimetre() {
+        return perimetre;
+    }
+
+    public int getArea() {
+        return area;
     }
 
 }
