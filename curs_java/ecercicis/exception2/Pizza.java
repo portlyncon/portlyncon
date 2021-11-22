c6a7b63d69aa5212b179734a8f437a8ac1e42683
portlyncon <supergatchan@hotmail.com>
2021-11-22 12:33:20 +0000

    fefefefef

--- curs_java/ecercicis/exception2/Pizza.java
+++ curs_java/ecercicis/exception2/Pizza.java
@@ -37,4 +37,7 @@ public class Pizza {
 
     }
 
+    public double getPrecio() {
+        return precio;
+    }
 }
--- curs_java/ecercicis/exception2/prova.java
+++ curs_java/ecercicis/exception2/prova.java
@@ -1,16 +1,74 @@
 package curs_java.ecercicis.exception2;
 
-import curs_java.Universitat.practica2.EstatuetSalmeronIsaac_POO_Praactica2.src.Main;
-import curs_java.ecercicis.herencia.exercici1.main;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class prova {
+
     public static void main(String[] args) {
 
-        try {
+        ArrayList<Pizza> lista = new ArrayList<>();
+        Pizza pizza_nova = new Pizza("prova", 3, 3.5);
+        Pizzeria pi = new Pizzeria(lista);
+        pi.Lista_pizzas.add(pizza_nova);
+        String nombre_pizza = "";
+        int cantidad_pizzas = 0;
+        int cantidad_queso = 0;
+        double precio = 0.0;
+        PizzaComanda pi_co = new PizzaComanda(nombre_pizza, cantidad_queso, precio, cantidad_pizzas);
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        double total_comanda = 0.0;
 
-        } catch (Pizza_error e) {
-            // TODO: handle exception
+        while (!salir) {
+
+            System.out.println("1. Consulta Lista de Pizzas");
+            System.out.println("2. Hacer pedido Pizza");
+            System.out.println("3. Finalizar Comanda");
+            int op = sn.nextInt();
+            switch (op) {
+            case 1:
+                sn.nextLine();
+                try {
+
+                    for (Pizza pizza : pi.Lista_pizzas) {
+
+                        System.out.println("nombre: " + pizza.getNombre() + "precio: " + pizza.getPrecio());
+                    }
+                } catch (Pizza_error e) {
+                    System.out.println(e.getMessage());
+                }
+
+                break;
+            case 2:
+                sn.nextLine();
+                System.out.println("Que pizza quieres comprar..");
+                nombre_pizza = sn.nextLine();
+                sn.nextLine();
+                System.out.println("Que cantidad de queso quieres...");
+                cantidad_queso = sn.nextInt();
+                sn.nextLine();
+                System.out.println("Que cantidad de pizzas quieres...");
+                cantidad_pizzas = sn.nextInt();
+                sn.nextLine();
+                try {
+                    pi.comanda(pi_co);
+
+                } catch (Pizza_error e) {
+                    // TODO: handle exception
+                }
+
+                total_comanda = total_comanda + pi_co.getPrecio();
+                break;
+            case 3:
+
+                System.out.println("Total Comanda: " + total_comanda);
+                salir = true;
+                break;
+            }
         }
+        sn.close();
 
     }
+
 }
