87d75e305814dd482c7b756d5505cb28b6d7d700
portlyncon <59870519+portlyncon@users.noreply.github.com>
2021-12-13 18:57:37 +0100

    dfsd

--- curs_java/ecercicis/exerciciEnum/exercici2Enum.java
+++
@@ -1,6 +0,0 @@
-package curs_java.ecercicis.exerciciEnum;
-
-public class exercici2Enum {
-    // cal crear metode per retornar un int per cada enum correponent al dia de la
-    // setmana i depsres sumar o restar en funcio del que demanin
-}
