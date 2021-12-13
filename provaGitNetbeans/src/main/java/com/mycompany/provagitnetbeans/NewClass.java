910b103ff6b9f6a7983995fe8c611be62cb928c2
portlyncon <supergatchan@hotmail.com>
2021-12-13 13:58:34 +0000

    creacio projecte maiven Git prova Signed-off-by:portlyncon <supergatchan@hotmail.com>

--- provaGitNetbeans/src/main/java/com/mycompany/provagitnetbeans/NewClass.java
+++ provaGitNetbeans/src/main/java/com/mycompany/provagitnetbeans/NewClass.java
@@ -11,5 +11,6 @@ package com.mycompany.provagitnetbeans;
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("com.mycompany.provagitnetbeans.NewClass.main()");
+        System.err.println("fefwefwe");
     }
 }
