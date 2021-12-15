8adae5ae081b729d9878964c01e3f6bc7a02dd75
portlyncon <59870519+portlyncon@users.noreply.github.com>
2021-12-15 21:40:37 +0100

    ggtrg

---
+++ curs_java/ecercicis/Test.java
@@ -0,0 +1,21 @@
+package curs_java.ecercicis;
+
+public class Test {
+    
+
+    public static void main(String[] args) {
+
+        int x = 100;
+    
+        int a = x++;
+    
+        int b = ++x;
+    
+        int c = x++;
+    
+        int d = (a < b) ? (a < c) ? a : ((b < c) ? b : c);
+    
+        System.out.println(d);
+    
+    }
+}
