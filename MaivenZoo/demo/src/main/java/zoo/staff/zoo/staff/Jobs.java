e92de45c9806e424567eb170a3c4bc04aecc5d5d
portlyncon <59870519+portlyncon@users.noreply.github.com>
2022-01-19 20:54:24 +0100

    zoo

---
+++ MaivenZoo/demo/src/main/java/zoo/staff/zoo/staff/Jobs.java
@@ -0,0 +1,12 @@
+package zoo.staff;
+
+import zoo.animal.feeding.Task;
+
+public class Jobs { 
+
+    public static void main(String[] args) {
+        System.out.println("Zoo Staff!!!!");
+
+        Task.main();
+    }
+}
